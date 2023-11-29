073e7770015ce468c4a7a316d334ea3442cdde94
Nazneen <112861261+Nazneen05x@users.noreply.github.com>
2023-11-29 12:04:41 +0100

    code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,9 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import BlogBorder from "$lib/atoms/blogBorder.svelte";
+  import BlogButton from "$lib/atoms/blogButton.svelte";
+  import BlogHex from "$lib/atoms/blogHex.svelte";
+</script>
+
+<BlogBorder />
+<BlogButton />
+<BlogHex />
